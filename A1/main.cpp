7462375c96248ebcd106c665f5ea9d782f8bbd0b
Alex00Antone <alexjantone@gmail.com>
2025-01-16 15:39:49 -0700

    I hate vscode

---
+++ A1/main.cpp
@@ -0,0 +1,28 @@
+/* CSCI 200: Assignment 1: A1 - Random Encounter RPG
+ *
+ * Author: XXXX (INSERT_NAME)
+ * Resources used (Office Hours, Tutoring, Other Students, etc & in what capacity):
+ *     // list here any outside assistance you used/received while following the
+ *     // CS@Mines Collaboration Policy and the Mines Academic Code of Honor
+ *
+ * XXXXXXXX (MORE_COMPLETE_DESCRIPTION_HERE)
+ */
+// The include section adds extra definitions from the C++ standard library.
+#include <iostream> // For cin, cout, etc.
+
+// We will (most of the time) use the standard library namespace in our programs.
+using namespace std;
+
+// Define any constants below this comment.
+
+// Must have a function named "main", which is the starting point of a C++ program.
+int main() {
+
+  /******** INSERT YOUR CODE BELOW HERE ********/
+
+  cout << "Hello world!" << endl; // print Hello world! to the screen
+
+  /******** INSERT YOUR CODE ABOVE HERE ********/
+
+  return 0; // signals the operating system that our program ended OK.
+}
